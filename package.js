b357a9fdd48d416ae937711b4993de1fb1afa6a4
mik393@tut.by <mik393@tut.by>
2014-07-25 11:39:50 +0300

    Adding css for combobox

--- package.js
+++ package.js
@@ -3,5 +3,5 @@ Package.describe({
 });
 
 Package.on_use(function (api, where) {
-  api.add_files('js/jquery.scombobox.min.js', 'client');
+  api.add_files(['js/jquery.scombobox.min.js', 'css/jquery.scombobox.min.css'], 'client');
 });
